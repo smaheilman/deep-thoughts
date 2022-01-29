cc8e6bb6d9149e004f8959fec1f20071e0011399
Shmee <smaheilman@gmail.com>
2022-01-28 23:01:50 -0500

    client auth

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -13,9 +16,21 @@ const Login = (props) => {
     });
   };
 
+  const [login, { error }] = useMutation(LOGIN_USER);
+
   // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+
+    try {
+      const { data } = await login({
+        variables: { ...formState }
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
 
     // clear form values
     setFormState({
@@ -52,6 +67,7 @@ const Login = (props) => {
               <button className='btn d-block w-100' type='submit'>
                 Submit
               </button>
+              {error && <div>Login failed</div>}
             </form>
           </div>
         </div>
